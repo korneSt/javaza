5c4efd945be5be6aaeaf20d3428662c682200098
mariopce <mariopce@gmail.com>
2013-04-19 23:57:25 +0200

    Finish task, test started

--- zad2/src/main/java/pl/juglodz/szkolenie/App.java
+++ zad2/src/main/java/pl/juglodz/szkolenie/App.java
@@ -1,13 +1,25 @@
 package pl.juglodz.szkolenie;
 
+import pl.juglodz.szkolenie.operation.Operation;
+import pl.juglodz.szkolenie.operation.OperationFactory;
+
 /**
- * Hello world!
- *
+ * Napisz program, ktory pobiera dwie liczby oraz lancuch znakow z wiersza
+ * polecen oraz wyswietla fragment podanego lancucha okreslony wprowadzonymi
+ * liczbami. Na przyklad:
+ * 
+ * java Substring Witaj 2 4
+ * 
+ * powinien wyświetlic:
+ * 
+ * taj Obsłuż wszystkie mozliwe wyjatki, ktore moga wystapic w przypadku złego
+ * zestawu argumentow.
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Operation operation = OperationFactory.create(args);
+		String result = operation.execute();
+		System.out.println("Result:: " + result);
+	}
 }
--- zad2/src/main/java/pl/juglodz/szkolenie/operation/OperationFactory.java
+++ zad2/src/main/java/pl/juglodz/szkolenie/operation/OperationFactory.java
@@ -1,41 +1,40 @@
+
 package pl.juglodz.szkolenie.operation;
 
 import com.google.common.base.Strings;
 
 public class OperationFactory {
 
-	private static final String OPERATION_CONCAT = "concat";
-	private static final String OPERATION_SUBSTRING = "substring";
-	private static final int OPERATION_NAME_ID = 1;
-	private static final int MIN_ARG_SIZE = 3;
+    private static final String OPERATION_CONCAT = "concat";
+    private static final String OPERATION_SUBSTRING = "substring";
+    private static final int OPERATION_NAME_ID = 0;
+    private static final int MIN_ARG_SIZE = 3;
 
-	public static Operation create(final String[] args) {
-		if (args == null) {
-			throw new NullPointerException("empty list of arguments");
-		}
-		// Don't use magic numbers
-		if (args.length < MIN_ARG_SIZE) {
-			throw new IllegalStateException("min arg size need be gt "
-					+ MIN_ARG_SIZE + " currentlly " + args.length);
-		}
-		// Give me operation
-		String operationName = args[OPERATION_NAME_ID];
-		Operation o = createOperation(operationName, args);
-		return o;
-	}
+    public static Operation create(final String[] args) {
+        if (args == null) {
+            throw new NullPointerException("empty list of arguments");
+        }
+        // Don't use magic numbers
+        if (args.length < MIN_ARG_SIZE) {
+            throw new IllegalStateException("min arg size need be gt " + MIN_ARG_SIZE + " currentlly " + args.length);
+        }
+        // Give me operation
+        String operationName = args[OPERATION_NAME_ID];
+        Operation o = createOperation(operationName, args);
+        return o;
+    }
 
-	private static Operation createOperation(String operationName, String[] args) {
-		if (Strings.isNullOrEmpty(operationName)) {
-			throw new IllegalStateException("Operation name can't be null");
-		}
-		if (OPERATION_SUBSTRING.equalsIgnoreCase(operationName)) {
-			return SubstringOperation.create(args);
-		} else if (OPERATION_CONCAT.equalsIgnoreCase(operationName)) {
-			return ConcatOperation.create(args);
-		} else {
-			throw new UnsupportedOperationException("Operation "
-					+ operationName + " not supported");
-		}
-	}
+    private static Operation createOperation(String operationName, String[] args) {
+        if (Strings.isNullOrEmpty(operationName)) {
+            throw new IllegalStateException("Operation name can't be null");
+        }
+        if (OPERATION_SUBSTRING.equalsIgnoreCase(operationName)) {
+            return SubstringOperation.create(args);
+        } else if (OPERATION_CONCAT.equalsIgnoreCase(operationName)) {
+            return ConcatOperation.create(args);
+        } else {
+            throw new UnsupportedOperationException("Operation " + operationName + " not supported");
+        }
+    }
 
 }

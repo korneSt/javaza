528dac92dfb48a3574fadbdf8429650462306f6d
mariopce <mariopce@gmail.com>
2013-05-26 13:13:04 +0200

    Link Activity

---
+++ Notatnik/src/pl/juglodz/notatnik/LinkSearcherActivity.java
@@ -0,0 +1,14 @@
+package pl.juglodz.notatnik;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LinkSearcherActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.link_main);
+	}
+
+}
